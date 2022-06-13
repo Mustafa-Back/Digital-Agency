418a258ea203325eec0a7e15252704da39162cbf
Mustafa Back <alha3be@gmail.com>
2022-06-13 11:35:47 +0200

    Enter

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,5 +1,9 @@
 const menu = document.querySelector(".menu");
 const nav = document.querySelector("nav");
+const header = document.querySelector("header");
 menu.addEventListener("click", function () {
   nav.classList.toggle("o-nav");
+  nav.classList.contains("o-nav")
+    ? (header.style.background = "#f6f6f6 ")
+    : (header.style.background = "transparent");
 });
