16328fe58d11c23b0e4d57ab55a11cdcf7956036
Mustafa Back <alha3be@gmail.com>
2022-06-14 14:25:13 +0200

    Enter

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,9 +1,15 @@
 const menu = document.querySelector(".menu");
 const nav = document.querySelector("nav");
 const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  const scrolledY = this.window.scrollY;
+  // console.log(`---${scrolledY}---`);
+  scrolledY >= 50
+    ? header.classList.add("header")
+    : header.classList.remove("header");
+});
+
 menu.addEventListener("click", function () {
   nav.classList.toggle("o-nav");
-  nav.classList.contains("o-nav")
-    ? (header.style.background = "#f6f6f6 ")
-    : (header.style.background = "transparent");
 });
